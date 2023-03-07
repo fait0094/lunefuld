4a886bf3dd63add98a9374d67a13706b3585cd1e
Faith Mansilungan <faith0094@stud.kea.dk>
2023-03-07 09:43:52 +0100

    Cookieboks

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+if (sessionStorage.getItem(fjernCookieBoks)) {
+  cookieboks.classList.add("hidden");
+}
